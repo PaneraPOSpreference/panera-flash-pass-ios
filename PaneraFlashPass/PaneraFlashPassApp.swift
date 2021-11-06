785b47284c0263ba80465621374cadf74cbc223c
Jacob Logan Woods <mrwoodsjacob45@gmail.com>
2021-11-05 20:49:30 -0500

    Initial Commit

---
+++ PaneraFlashPass/PaneraFlashPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaneraFlashPassApp.swift
+//  PaneraFlashPass
+//
+//  Created by Jacob Woods on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct PaneraFlashPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
