aa613b681a2e4d2bcb76df371c93f5ca52d34271
Jacob Logan Woods <mrwoodsjacob45@gmail.com>
2021-11-05 22:07:49 -0500

    Added Fonts

--- PaneraFlashPass/ContentView.swift
+++ PaneraFlashPass/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+    
+    VStack{
+    PaneraLogo()
         
-            
+    Welcome(username: "Jacob Woods")
+    Spacer()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13")
+        
     }
+}
 }
--- PaneraFlashPass/UserView.swift
+++ PaneraFlashPass/UserView.swift
@@ -13,6 +13,33 @@ struct UserView: View {
     }
 }
 
+struct PaneraText: View{
+    var input: String
+    var textSize: Int
+    var body: some View{
+        Text(input)
+        .font(Font.custom("panera", size: CGFloat(textSize)))    }
+          
+    }
+struct Welcome:View{
+    var username: String
+    var body: some View{
+        Text("Hello, \(username)")
+            .font(Font.custom("roboto-medium", size: 30))
+    }
+}
+
+struct PaneraLogo:View{
+    var body: some View{
+        VStack{
+        PaneraText(input: "Panera", textSize: 40)
+        PaneraText(input: "Flash Pass", textSize: 30)
+                .foregroundColor(Color("PaneraGreen"))
+        }
+    }
+}
+
+
 struct UserView_Previews: PreviewProvider {
     static var previews: some View {
         UserView()
